52cc59e00bce742253b66a513a150d5c6941bdc7
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2023-11-01 21:49:45 +0100

    Inital design + projects list

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "main-layout": "max(200px, 30%) 1fr",
+      },
+    },
+  },
+  plugins: [],
+}
+
