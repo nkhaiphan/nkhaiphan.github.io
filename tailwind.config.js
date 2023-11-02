b6fd3aa8de6b43a227735da8e9f462b1efc8a10e
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2023-11-02 20:46:55 +0100

    Visual update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,24 @@ module.exports = {
       },
       textUnderlineOffset: {
         16: "16px",
+      },
+      keyframes: {
+        "fade-in-tb": {
+          "0%": { opacity: 0, transform: "translateY(-20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" }
+        },
+        "fade-in": {
+          "0%": { opacity: 0},
+          "100%": { opacity: 1}
+        },
+      },
+      animation: {
+        "fade-in-tb": "fade-in-tb 1.5s ease-in-out",
+        "fade-in-tb-2": "fade-in-tb 2s ease-in-out",
+        "fade-in-tb-3": "fade-in-tb 3s ease-in-out",
+        "fade-in": "fade-in 1.5s ease-in-out",
+        "fade-in-2": "fade-in 2s ease-in-out",
+        "fade-in-3": "fade-in 3s ease-in-out",
       }
     },
   },
